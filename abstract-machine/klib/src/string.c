562afe6e5059be55f4097c84bec4e8ce0479d305
yizishun <320016@qq.com>
2024-01-27 11:18:34 +0800

    klib1

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,23 +5,43 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+	size_t len;
+	for(len = 0; s[len] != '\0' ; len++);
+	return len;
 }
 
-char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+char *strcpy(char *dst, const char *src) { 
+  size_t i;
+	for (i = 0; src[i] != '\0'; i++)
+  	dest[i] = src[i];
+  dest[i] = '\0';
+  return dest;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  size_t i;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+  	dest[i] = src[i];
+  for ( ; i < n; i++)
+    dest[i] = '\0';
+  return dest;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  size_t dest_len = strlen(dest);
+  size_t i;
+  for (i = 0 ; src[i] != '\0' ; i++)
+  	dest[dest_len + i] = src[i];
+  dest[dest_len + i] = '\0';
+	return dest;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  int flag,i;
+	for(i = 0; s1[i] != '\0' && s2[i] != '\0' ; i++){
+		flag = s1[i] - s2[i];
+
+	}
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
@@ -29,7 +49,7 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  panic("Not implemented1");
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -41,7 +61,7 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  panic("Not implemented1");
 }
 
 #endif
