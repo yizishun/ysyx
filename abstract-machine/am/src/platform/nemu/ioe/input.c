ce7f13033b3d44952464e71378bf1e7e464483df
ysyx_23060171_yzs <320016@qq.com>
2024-03-12 08:40:08 +0800

    NEMU keyboard am complte and can operate mario

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,11 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  kbd->keycode = inl(KBD_ADDR);
+  if(kbd->keycode == AM_KEY_NONE || (kbd->keycode >> 15) == 0)
+    kbd->keydown = 0;
+  else{
+    kbd->keydown = 1;
+    kbd->keycode = kbd->keycode & ~KEYDOWN_MASK;
+    }
 }
