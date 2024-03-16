247771352fbb64d8b13428f7e221e3f06d959b1a
ysyx_23060171_yzs <320016@qq.com>
2024-03-16 08:45:23 +0800

    NJU-ProjectN/navy-apps ics2023 initialized
    NJU-ProjectN/navy-apps e8346911c80af39b63e47d8aba5878e43231ad63 libos,native: add comment for the format of /proc/dispinfo

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,6 +2,9 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
+    case EVENT_YIELD:
+      printf("yield\n");
+      break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -23,7 +23,7 @@ int mm_brk(uintptr_t brk) {
 
 void init_mm() {
   pf = (void *)ROUNDUP(heap.start, PGSIZE);
-  Log("free physical pages starting from %p", pf);
+  Log("free physical pages starting from %p\n", pf);
 
 #ifdef HAS_VME
   vme_init(pg_alloc, free_page);
