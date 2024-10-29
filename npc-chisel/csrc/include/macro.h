d2742940466f50e8299edb0bca0654f7c60ce20f
ysyx_23060171_yzs <320016@qq.com>
2024-10-29 19:26:21 +0800

    Remove npc-chisel

--- npc-chisel/csrc/include/macro.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MACRO_H__
-#define __MACRO_H__
-
-// calculate the length of an array
-#define ARRLEN(arr) (int)(sizeof(arr) / sizeof(arr[0]))
-#define STR_HELPER(x) #x
-#define STR(x) STR_HELPER(x)
-#endif
--- npc-chisel/src/main/Core/IDU/Ebreak.sv
+++
@@ -1,9 +0,0 @@
-module Ebreak(
-  input isEbreak
-);
-  import "DPI-C" function void npc_trap();
-  always @(*) begin
-    if(isEbreak)
-      npc_trap();
-  end
-endmodule
